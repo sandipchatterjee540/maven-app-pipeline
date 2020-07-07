4d526fdeb76792dc43f0bbce4c57277c712b472b
sandipchatterjee540 <58181993+sandipchatterjee540@users.noreply.github.com>
2020-07-07 13:37:06 +0530

    Update mvn.sh

--- jenkins-script/build/mvn.sh
+++ jenkins-script/build/mvn.sh
@@ -6,4 +6,7 @@ echo "*****************Build Jar File ******************************************
 echo "******************************************************************************"
 
 
-docker run --rm -it -v $PWD/java-app/:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
+WORKSPACE=/var/jenkins_home/workspace/PULL-FROM-GIT-PUSH-IN-DOCKER-HUB-MAVEN-APP
+
+
+docker run --rm -it -v $WORKSPACE/java-app/:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
