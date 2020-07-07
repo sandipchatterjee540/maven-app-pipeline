4e3b89d65960f12a74330c6110e1934b439ea55e
sandipchatterjee540 <58181993+sandipchatterjee540@users.noreply.github.com>
2020-07-07 14:39:59 +0530

    Update test.sh

--- jenkins-script/test/test.sh
+++ jenkins-script/test/test.sh
@@ -5,5 +5,6 @@ echo "**************************************************************************
 echo "***************** Testing The Code **********************************************"
 echo "******************************************************************************"
 
+WORKSPACE=/home/sandip/jenkins/jenkins_data/workspace/PULL-FROM-GIT-PUSH-IN-DOCKER-HUB-MAVEN-APP
 
-docker run --rm -v $PWD/java-app/:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
